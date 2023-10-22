0a24e3c9f64b231459e6cbd68e0c0f79364e4d88
YoannDrx <yoann.andrieux@gmail.com>
2023-10-22 16:17:53 +0200

    Home > ArticleCard > use Next Image

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,10 +1,19 @@
+import Image from "next/image";
+
 export default function ArticleCard({backgroundImage, date, title}) {
 	const day = new Date(date).getDate();
 	const month = new Date(date).toLocaleString("fr-FR", {month: "short"}).toUpperCase();
 
 	return (
-		<div className="relative w-80 h-64 bg-cover bg-center" style={{backgroundImage: `url(${backgroundImage})`}}>
-			<div className="absolute bottom-0 w-full h-1/4 bg-black bg-opacity-70 flex justify-between items-center">
+		<div className="relative w-80 h-64 overflow-hidden">
+			<Image
+				src={backgroundImage}
+				alt="Article Background"
+				className="absolute w-full h-full object-cover"
+				layout="fill"
+				objectFit="cover"
+			/>
+			<div className="absolute bottom-0 w-full h-1/4 bg-black bg-opacity-80 flex justify-between items-center">
 				<span className="text-white px-4">{title}</span>
 				<div className="bg-primary w-16 h-16 flex flex-col justify-center items-center right-0">
 					<span className=" text-white font-bold text-lg">{day}</span>
