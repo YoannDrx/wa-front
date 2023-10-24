fd667ec6bc561287bb0d9d89aa6254026e25eabf
YoannDrx <yoann.andrieux@gmail.com>
2023-10-24 21:37:40 +0200

    Home > ArticleCard > comportement hover desktop vs mobile

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,41 +1,38 @@
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import { useState } from "react";
 import Button from "./Button";
 import { t } from "i18next";
 
 export default function ArticleCard({ backgroundImage, date, title, href }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
   const day = new Date(date).getDate();
   const month = new Date(date).toLocaleString("fr-FR", { month: "short" }).toUpperCase().replace(".", "");
-  const transitionClass = "transition-all duration-500";
-  const bottomDivClass = `absolute bottom-0 left-0 w-full h-1/4 bg-black bg-opacity-80 ${transitionClass} ${
-    isHovered ? "h-full" : "h-1/4"
-  }`;
 
-  const renderDate = () => (
-    <div className="bg-primary min-w-16 min-h-16 flex flex-col justify-center items-center px-4">
-      <h3 className="text-white font-bold text-xl">{day}</h3>
-      <h3 className="text-white text-xs">{month}</h3>
-    </div>
-  );
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 400);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-  const renderHoverContent = () => (
-    <div className="text-white p-4 flex flex-col justify-between h-3/4 pt-2">
-      <div className="overflow-hidden line-clamp-5 text-gray-300 text-sm">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam asperiores iste totam vero earum aliquid ducimus vel
-        dolor delectus distinctio et, facere quos illum temporibus recusandae illo pariatur, ad labore.
-      </div>
-      <Button href={href} color="primary" className="self-center mt-4 w-2/5">
-        {t("En savoir plus")}
-      </Button>
-    </div>
-  );
+  const toggleHover = () => {
+    if (isMobile) {
+      setIsHovered(!isHovered);
+    }
+  };
 
   return (
     <div
       className="relative w-80 h-64 overflow-hidden cursor-pointer"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}>
+      onMouseEnter={() => !isMobile && setIsHovered(true)}
+      onMouseLeave={() => !isMobile && setIsHovered(false)}
+      onClick={toggleHover}>
       <Image
         src={backgroundImage}
         alt="Article Background"
@@ -43,14 +40,30 @@ export default function ArticleCard({ backgroundImage, date, title, href }) {
         layout="fill"
         objectFit="cover"
       />
-      <div className={bottomDivClass}>
+      <div
+        className={`absolute bottom-0 left-0 w-full h-1/4 bg-black bg-opacity-80 transition-all duration-500 ${
+          isHovered ? "h-full" : "h-1/4"
+        }`}>
         <div className="flex justify-between items-center h-16">
           <div className="flex flex-grow items-center pl-2">
             <h3 className="text-white text-xs">{title}</h3>
           </div>
-          {renderDate()}
+          <div className="bg-primary min-w-16 min-h-16 flex flex-col justify-center items-center px-4">
+            <h3 className="text-white font-bold text-xl">{day}</h3>
+            <h3 className="text-white text-xs">{month}</h3>
+          </div>
         </div>
-        {isHovered && renderHoverContent()}
+        {isHovered && (
+          <div className="text-white p-4 flex flex-col justify-between h-3/4 pt-2">
+            <div className="overflow-hidden line-clamp-5 text-gray-300 text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam asperiores iste totam vero earum aliquid ducimus
+              vel dolor delectus distinctio et, facere quos illum temporibus recusandae illo pariatur, ad labore.
+            </div>
+            <Button href={href} color="primary" className="self-center mt-4 w-2/5">
+              {t("En savoir plus")}
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
