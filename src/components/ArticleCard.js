cabe5e9da1b1c8de93b024cb6b0bcb6858da5766
YoannDrx <yoann.andrieux@gmail.com>
2023-10-24 21:27:47 +0200

    Home > refacto AricleCard

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -7,6 +7,29 @@ export default function ArticleCard({ backgroundImage, date, title, href }) {
   const [isHovered, setIsHovered] = useState(false);
   const day = new Date(date).getDate();
   const month = new Date(date).toLocaleString("fr-FR", { month: "short" }).toUpperCase().replace(".", "");
+  const transitionClass = "transition-all duration-500";
+  const bottomDivClass = `absolute bottom-0 left-0 w-full h-1/4 bg-black bg-opacity-80 ${transitionClass} ${
+    isHovered ? "h-full" : "h-1/4"
+  }`;
+
+  const renderDate = () => (
+    <div className="bg-primary min-w-16 min-h-16 flex flex-col justify-center items-center px-4">
+      <h3 className="text-white font-bold text-xl">{day}</h3>
+      <h3 className="text-white text-xs">{month}</h3>
+    </div>
+  );
+
+  const renderHoverContent = () => (
+    <div className="text-white p-4 flex flex-col justify-between h-3/4 pt-2">
+      <div className="overflow-hidden line-clamp-5 text-gray-300 text-sm">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam asperiores iste totam vero earum aliquid ducimus vel
+        dolor delectus distinctio et, facere quos illum temporibus recusandae illo pariatur, ad labore.
+      </div>
+      <Button href={href} color="primary" className="self-center mt-4 w-2/5">
+        {t("En savoir plus")}
+      </Button>
+    </div>
+  );
 
   return (
     <div
@@ -20,30 +43,14 @@ export default function ArticleCard({ backgroundImage, date, title, href }) {
         layout="fill"
         objectFit="cover"
       />
-      <div
-        className={`absolute bottom-0 left-0 w-full h-1/4 bg-black bg-opacity-80 transition-all duration-500 ${
-          isHovered ? "h-full" : "h-1/4"
-        }`}>
+      <div className={bottomDivClass}>
         <div className="flex justify-between items-center h-16">
           <div className="flex flex-grow items-center pl-2">
             <h3 className="text-white text-xs">{title}</h3>
           </div>
-          <div className="bg-primary min-w-16 min-h-16 flex flex-col justify-center items-center px-4">
-            <h3 className="text-white font-bold text-xl">{day}</h3>
-            <h3 className="text-white text-xs">{month}</h3>
-          </div>
+          {renderDate()}
         </div>
-        {isHovered && (
-          <div className="text-white p-4 flex flex-col justify-between h-3/4 pt-2">
-            <div className="overflow-hidden line-clamp-5 text-gray-300 text-sm">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam asperiores iste totam vero earum aliquid ducimus
-              vel dolor delectus distinctio et, facere quos illum temporibus recusandae illo pariatur, ad labore.
-            </div>
-            <Button href={href} color="primary" className="self-center mt-4 w-2/5">
-              {t("En savoir plus")}
-            </Button>
-          </div>
-        )}
+        {isHovered && renderHoverContent()}
       </div>
     </div>
   );
