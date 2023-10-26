661b6752e3b8f4a0cb152cc2e32463c45bad7419
YoannDrx <yoann.andrieux@gmail.com>
2023-10-26 20:56:05 +0200

    Partenaires > add pictures

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -4,11 +4,11 @@ import React from "react";
 export default function TeamCard({ image, name, references, description }) {
   return (
     <div className="bg-white shadow-lg">
-      <Image src={image} alt={`${name}`} className="w-full object-cover h-48" width={200} height={500} />
+      <Image src={image} alt={`${name}`} className="w-full object-cover h-auto" width={200} height={500} />
       <div className=" text-center">
         <h2 className="text-sm underblue mt-2">{name}</h2>
-        <p className="text-sm m-0">{references}</p>
-        <p className="text-xs text-gray-500 mb-2">{description}</p>
+        <h3 className="text-xs p-2">{references}</h3>
+        <p className="text-xs text-gray-500">{description}</p>
       </div>
     </div>
   );
--- src/pages/partenaires.js
+++ src/pages/partenaires.js
@@ -8,31 +8,31 @@ export default function Partners() {
 
   const teamMembers = [
     {
-      image: "/assets/team/test.png",
+      image: "/assets/team/Bruno-Weil.png",
       name: "BRUNO WEIL",
       references: "AVOCAT (1999)",
       description: "LL.M. European Business Law (Pallas Consortium)",
     },
     {
-      image: "/assets/team/test.png",
+      image: "/assets/team/Eric-Weil.png",
       name: "ERIC WEIL",
       references: "Avocat (1996), Rechtsanwalt (1999), Attorney at Law (2002)",
       description: "LL.M. (Berlin et Duke-Law)",
     },
     {
-      image: "/assets/team/test.png",
+      image: "/assets/team/Patricia-Vincent.png",
       name: "PATRICIA VINCENT",
       references: "Avocat (2005)",
       description: "LL.M. (Postdam)",
     },
     {
-      image: "/assets/team/test.png",
+      image: "/assets/team/Mathilde-Houet-Weil.png",
       name: "MATHILDE HOUET-WEIL",
       references: "Avocat (1996), Attorney at Law (2002)",
       description: "LL.M. (Duke-Law)",
     },
     {
-      image: "/assets/team/test.png",
+      image: "/assets/team/Heinz-Weil.png",
       name: "HEINZ WEIL",
       references: "Avocat (1986), Rechtsanwalt (1971)",
       description: "Ancien Président du Conseil des Barreaux de l'Union Européenne (CCBE)",
