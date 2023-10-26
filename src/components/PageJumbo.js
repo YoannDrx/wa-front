ae9f79ecf1453b3f09ade580b54a4abfa3f74241
YoannDrx <yoann.andrieux@gmail.com>
2023-10-26 21:02:06 +0200

    PageJumbo > add background color

--- src/components/PageJumbo.js
+++ src/components/PageJumbo.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Trans, useTranslation } from "react-i18next";
 
-export default function PageJumbo({ titleKey, textKey }) {
+export default function PageJumbo({ titleKey, textKey, backgroundColor }) {
   const { t } = useTranslation();
+
+  const containerStyle = {
+    backgroundColor: backgroundColor ? backgroundColor : "#FFF",
+  };
+
   return (
-    <div className="expertise-background">
-      <div className="container text-center pt-24">
+    <div style={containerStyle}>
+      <div className="container text-center pt-24 pb-12">
         <h1 className="underblue">{t(titleKey)}</h1>
         {textKey && (
           <p>
