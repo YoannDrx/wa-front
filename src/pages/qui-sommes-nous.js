7273afb94640d466516d636ad59fdf3b4f9c3c17
YoannDrx <yoann.andrieux@gmail.com>
2023-10-26 23:43:50 +0200

    WIP : who we are

--- src/pages/qui-sommes-nous.js
+++ src/pages/qui-sommes-nous.js
@@ -1,49 +1,52 @@
 import React, { useEffect } from "react";
 import PageJumbo from "@/components/PageJumbo";
-import { Trans, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { t } from "i18next";
+import Image from "next/image";
+import Section from "@/components/Section";
 
 export default function WhoWeAre() {
   const { i18n } = useTranslation();
   useEffect(() => {}, [i18n.language]);
 
+  const renderSection = (titleKey, descKey, bgColor, textColor, colSpan = 1, titleColor) => (
+    <Section
+      title={t(titleKey)}
+      descriptionKey={descKey}
+      bgColor={bgColor}
+      textColor={textColor}
+      colSpan={colSpan}
+      titleColor={titleColor}
+      className="my-4"
+    />
+  );
+
   return (
     <div>
       <PageJumbo titleKey="PRESENTATION" textKey="firmPresentation" />
       <div className="container py-20">
-        <div className="flex flex-col">
-          {/* Section 1 */}
-          <div className="flex my-10">
-            <h2 className="text-2xl font-bold mr-10">{t("NOUS SOMMES DEDIES A VOTRE SUCCES")}</h2>
-            <Trans
-              i18nKey={"nous-sommes-dedies-a-votre-succes-description"}
-              components={[<span className="font-bold" key="0"></span>]}
-            />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-0">
+          {renderSection(
+            "NOUS SOMMES DEDIES A VOTRE SUCCES",
+            "nous-sommes-dedies-a-votre-succes-description",
+            "bg-primary",
+            "text-white",
+            2
+          )}
+          {renderSection("NOUS SOMMES INTERNATIONAUX", "nous-sommes-internationnaux-description", "bg-white", "text-black", 1)}
+
+          <div className="col-span-full my-4 relative w-full h-[400px]">
+            <Image src="/assets/who-we-are/salle-de-conference.png" alt="Séparateur" layout="fill" objectFit="cover" />
           </div>
-          {/* Section 2 */}
-          <div className="flex flex-row-reverse my-10">
-            <h2 className="text-2xl font-bold ml-10">{t("NOUS SOMMES INTERNATIONAUX")}</h2>
-            <Trans
-              i18nKey={"nous-sommes-internationnaux-description"}
-              components={[<span className="font-bold" key="0"></span>]}
-            />
-          </div>
-          {/* Section 3 */}
-          <div className="flex my-10">
-            <h2 className="text-2xl font-bold mr-10">{t("PLUS LOIN PLUS INTELLIGENT PLUS RAPIDE")}</h2>
-            <Trans
-              i18nKey={"plus-loin-plus-intelligent-plus-rapide-description"}
-              components={[<span className="font-bold" key="0"></span>]}
-            />
-          </div>
-          {/* Section 4 */}
-          <div className="flex flex-row-reverse my-10">
-            <h2 className="text-2xl font-bold ml-10">{t("VOTRE CONFIANCE NOUS HONORE")}</h2>
-            <Trans
-              i18nKey={"votre-confiance-nous-honore-description"}
-              components={[<span className="font-bold" key="0"></span>]}
-            />
-          </div>
+
+          {renderSection(
+            "PLUS LOIN PLUS INTELLIGENT PLUS RAPIDE",
+            "plus-loin-plus-intelligent-plus-rapide-description",
+            "#E4EDF1",
+            "#2E2E2E",
+            3
+          )}
+          {renderSection("VOTRE CONFIANCE NOUS HONORE", "votre-confiance-nous-honore-description", "#2E2E2E", "#E4EDF1", 3)}
         </div>
       </div>
     </div>
