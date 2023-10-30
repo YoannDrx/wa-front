74bb970cc57f6671bd2c0d5602c661c5692c810e
YoannDrx <yoann.andrieux@gmail.com>
2023-10-30 23:42:08 +0100

    article 1

--- src/pages/expertise/[expertise].js
+++ src/pages/expertise/[expertise].js
@@ -1,7 +1,5 @@
-import { useRouter } from "next/router";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { expertiseItems } from "@/data/expertiseData";
-import Image from "next/image";
 import PageJumbo from "@/components/PageJumbo";
 
 export async function getStaticPaths() {
@@ -23,33 +21,66 @@ export async function getStaticProps({ params }) {
 }
 
 export default function ExpertisePage({ expertise }) {
-  const router = useRouter();
   const { t } = useTranslation();
 
   return (
     <div className="container mx-auto px-4">
-      <PageJumbo titleKey={expertise.titleKey} textKey="Votre texte d'introduction ici." />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-12">
-        <div className="w-full h-full">
-          <Image src={expertise.image} alt={t(expertise.titleKey)} layout="responsive" width={700} height={500} />
-        </div>
-        <div className="text-lg leading-7 space-y-5">
-          <h2 className="text-2xl font-semibold">{t("Sous-titre 1")}</h2>
-          <p>{t("Texte détaillé sur cette section de l'expertise. Vous pouvez discuter des points clés, des avantages, etc.")}</p>
-        </div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-12">
-        <div className="text-lg leading-7 space-y-5">
-          <h2 className="text-2xl font-semibold">{t("Sous-titre 2")}</h2>
-          <p>
-            {t(
-              "Un autre paragraphe détaillé sur cette section de l'expertise. Vous pouvez discuter d'autres aspects importants ici."
-            )}
+      <PageJumbo titleKey={t(expertise.titleKey)} textKey={t(expertise.intro)} />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 my-12">
+        <div className="bg-white border-2 border-[#37749E] p-8">
+          <h2 className="text-2xl font-semibold text-primary">{t(expertise.titleBloc1)}</h2>
+          <p className="text-black">
+            <Trans
+              i18nKey={expertise.paragraph1}
+              components={{
+                nl: (
+                  <>
+                    <br />
+                    <br />
+                  </>
+                ),
+                bold: <span className="font-bold" key="0" />,
+                highlight: <span className="bg-yellow-400" key="1" />,
+              }}
+            />
           </p>
         </div>
-        <div className="w-full h-full">
-          <Image src={expertise.image} alt={t(expertise.titleKey)} layout="responsive" width={700} height={500} />
+        <div className="bg-[#37749E] p-8">
+          <h2 className="text-2xl font-semibold text-white">{t(expertise.titleBloc2)}</h2>
+          <p className="text-white">
+            <Trans
+              i18nKey={expertise.paragraph2}
+              components={{
+                nl: (
+                  <>
+                    <br />
+                    <br />
+                  </>
+                ),
+                bold: <span className="font-bold" key="0" />,
+                highlight: <span style={{ backgroundColor: "#F8F8F8", color: "grey" }} key="1" />,
+              }}
+            />
+          </p>
         </div>
+      </div>
+      <div className="bg-[#2E2E2E] p-8 my-12">
+        <h2 className="text-2xl font-semibold text-white">{t(expertise.titleBloc3)}</h2>
+        <p className="text-white">
+          <Trans
+            i18nKey={expertise.paragraph3}
+            components={{
+              nl: (
+                <>
+                  <br />
+                  <br />
+                </>
+              ),
+              bold: <span className="font-bold" key="0" />,
+              highlight: <span style={{ backgroundColor: "#37749E" }} key="1" />,
+            }}
+          />
+        </p>
       </div>
     </div>
   );
