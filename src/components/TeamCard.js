73902a2ce2b394d2b9971a8820b14685bc149406
YoannDrx <yoann.andrieux@gmail.com>
2023-11-10 00:06:44 +0100

    Refactor TeamCard component and add teamData

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -3,17 +3,21 @@ import Link from "next/link";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-export default function TeamCard({ image, name, references, description }) {
+export default function TeamCard({ image, name, references, description, id }) {
   const { t } = useTranslation();
   return (
     <div className="bg-white shadow-lg relative group">
-      <Link href={`/team/${name}`}>
+      <Link href={`/team/${id}`}>
         <Image src={image} alt={`${name}`} width={0} height={0} sizes="100vw" className="w-full h-auto" />
-        <div className="text-center">
-          <h3 className="underblue mt-2">{name}</h3>
-          <p className="font-bold">{references}</p>
-          <p className="text-gray-500">{description}</p>
+
+        <div className="text-center p-4 flex-1 flex flex-col justify-between">
+          <h3 className="underblue mt-2 text-lg">{name}</h3>
+          <div>
+            <p className="font-bold mt-2">{references}</p>
+            <p className="text-gray-500 mt-2 mb-4">{description}</p>
+          </div>
         </div>
+
         {/* Overlay Hover */}
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100">
           <button className="text-white font-bold">{t("partenaire.seeProfile")} →</button>
--- src/pages/team/[member].js
+++ src/pages/team/[member].js
@@ -3,25 +3,18 @@ import { useRouter } from "next/router";
 import PageJumbo from "@/components/PageJumbo";
 import Image from "next/image";
 import { FaEnvelope, FaPhone } from "react-icons/fa";
-
-const fetchMemberData = (memberName) => {
-  const mockData = {
-    name: memberName,
-    image: "/assets/team/Bruno-Weil.png",
-    title: "Avocat (1999)",
-    label: "LL.M. European Business - Law (Pallas Consortium)",
-    description:
-      'Being active since fifteen years as lawyer of mid-sized or international corporations which are present or active in France, Bruno Weil is your primary contact at our firm for any matter which deals with IP, IT or NTCs, his main field of expertise. As such, he intervenes as your advisor or counsel before courts for instances involving patent, trademark or copyright infringements, as well as any negotiation on license agreement or know-how transfer. In his capacity as counsel, he successfully represented several major US or German listed companies before French courts, notably in complex multimillion patent infringement lawsuits. Former correspondent for France of the editor KLUWER LAW INTERNATIONAL, as reporter for the "patent" section of the "EU IP CASES" database published by KLUWER under www.kluwereuipcases.com, he is also member of several international associations dealing with IP and new technologies, such as Licensing Executive Society - LES - Germany, GRUR and ITECHLAW. Bruno Weil is also active in other instances such as company restructuring or during takeovers. His day to day practice also involves court cases, for any business related lawsuits. His primary goal consists in successfully elaborating the soundest economical strategy for our client\'s interests to prevail. Besides French, he is fluent in German and English. An amateur drummer and percussionist, he is married and father of two daughters. Bruno recently published an article on the legal framework of trade secrecy in France : The protection of trade secrecy is strengthened under French law (2019)',
-    email: "bweil@weil-paris.fr",
-    phone: "+33 (0)1.44.15.98.98",
-  };
-  return mockData;
-};
+import { t } from "i18next";
+import { teamData } from "@/data/teamData";
 
 const Member = () => {
   const router = useRouter();
   const { member } = router.query;
-  const memberData = fetchMemberData(member);
+
+  const memberData = teamData.find((m) => m.id === member) || {};
+
+  if (!memberData.name) {
+    return <div>Membre non trouvé</div>;
+  }
 
   return (
     <div>
@@ -68,7 +61,16 @@ const Member = () => {
               </div>
             </div>
           </div>
-          <p className="mt-8">{memberData.description}</p>
+          <p className="mt-8">
+            {t(memberData.description)
+              .split("\n")
+              .map((line, index) => (
+                <React.Fragment key={index}>
+                  {line}
+                  <br />
+                </React.Fragment>
+              ))}
+          </p>
         </div>
       </div>
     </div>
