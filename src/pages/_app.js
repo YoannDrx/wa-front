14ab1b5f655971d7d7f2c20285b2c6f229f78976
YoannDrx <yoann.andrieux@gmail.com>
2023-11-30 23:24:38 +0100

    add react-cookie-consent pkg

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,8 +11,10 @@ import { CardProvider } from "../../contexts/ArticleCardContext";
 import axios from "axios";
 import { ArticlesProvider } from "../../contexts/ArticlesContext";
 import Head from "next/head";
+import CookieConsent from "react-cookie-consent";
 
-axios.defaults.baseURL = process?.env?.NODE_ENV === 'development' ? "http://localhost:3000/api" : "https://wa-front.vercel.app/api";
+axios.defaults.baseURL =
+  process?.env?.NODE_ENV === "development" ? "http://localhost:3000/api" : "https://wa-front.vercel.app/api";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
@@ -57,7 +59,23 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
       <CardProvider>
         <ArticlesProvider>{renderWithLayout(<Component {...pageProps} />)}</ArticlesProvider>
       </CardProvider>
+      <CookieConsent
+        location="bottom"
+        style={{ background: "#37749E", width: "100%" }}
+        buttonStyle={{ color: "white", backgroundColor: "green" }}
+        contentStyle={{ flex: "1" }}
+        buttonsContainerClasses="flex flex-col sm:flex-row justify-center items-center"
+        overlay={true}
+        expires={150}
+        acceptOnScroll={true}
+        buttonText={"J'accepte"}
+        enableDeclineButton={true}
+        declineButtonText={"Je refuse"}
+        onDecline={() => {
+          window.close();
+        }}>
+        Nous utilisons des cookies respectueux de vos données personnelles. Ils nous permettent d’améliorer votre expérience.
+      </CookieConsent>
     </>
-
   );
 }
